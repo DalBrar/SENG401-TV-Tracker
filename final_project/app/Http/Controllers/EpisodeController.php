46abd5dea949b07605726c7569d4eef5de87cdb7
Rebecca Senger <senger.becca@gmail.com>
2019-04-08 21:01:04 -0600

    new branch: added connection to episode More Details table and fixed watch/forget feature

--- final_project/app/Http/Controllers/EpisodeController.php
+++ final_project/app/Http/Controllers/EpisodeController.php
@@ -4,11 +4,65 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Episode;
 
 class EpisodeController extends Controller
 {
-    public function show($id)
+    public function show(Request $request)
     {
-        //
+      $episode = new Episode();
+      $episode->content_trakt_id = $request->input('content_trakt_id');
+      $episode->trakt_id = $request->input('trakt_id');
+      $episode->season = $request->input('season');
+      $episode->number = $request->input('number');
+
+      $ch = curl_init();
+
+      curl_setopt($ch,
+          CURLOPT_URL,
+          "https://api.trakt.tv/shows/" . $episode->content_trakt_id . "/seasons/" . $episode->season . "/episodes/" . $episode->number . "?extended=full"
+      );
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+      curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+      "Content-Type: application/json",
+      "trakt-api-version: 2",
+      "trakt-api-key: " . env('api_id')
+      ));
+
+      $response = curl_exec($ch);
+      curl_close($ch);
+
+      $jsonArray = json_decode($response, true);
+      $episodes = [];
+
+      $newepisode = new Episode([
+          'content_trakt_id' => $episode->content_trakt_id,
+          'season' => $jsonArray['season'],
+          'number' => $jsonArray['number'],
+          'title' => $jsonArray['title'],
+          'trakt_id' => $jsonArray['ids']['trakt'],
+          'overview' => $jsonArray['overview'],
+          'runtime' => $jsonArray['runtime'],
+          'first_aired' => $jsonArray['first_aired'],
+          'rating' => $jsonArray['rating'],
+      ]);
+      array_push($episodes, $newepisode);
+      $this->store($newepisode);
+
+      return view('episodes.show', ['episodes' => $episodes, 'options' => ['hideEpisodesButton' => true]]);
+    }
+
+    public function store(Episode $episode)
+    {
+      $existing_episode = Episode::where('trakt_id', $episode->trakt_id)->first();
+      if (is_null($existing_episode)) {
+          Episode::create([
+              'content_trakt_id' => $episode->content_trakt_id,
+              'trakt_id' => $episode->trakt_id
+          ]);
+
+      }
     }
 }
